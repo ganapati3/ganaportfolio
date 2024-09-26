3c8956e9c6beccba73012b3c0930596a12ff440b
ganapati3 <124158856+ganapati3@users.noreply.github.com>
2024-09-26 11:57:09 +0530

    update: config change for hash routing

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    historyApiFallback: true, // This ensures routes are handled by React Router
-  },
   base: isProduction ? '/ganaportfolio/' : '/',
 })
