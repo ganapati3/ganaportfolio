761da3ab685db92c5cea74be809864b6fd77e0f0
ganapati3 <124158856+ganapati3@users.noreply.github.com>
2024-09-26 20:26:01 +0530

    change:hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 // const router = createBrowserRouter([
 //   {
@@ -17,8 +17,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={isProduction ? '/ganaportfolio':'/'}>
-        <App/>
-      </BrowserRouter>
+    {/* <BrowserRouter basename={isProduction ? '/ganaportfolio':'/'}> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+      {/* </BrowserRouter> */}
   </StrictMode>,
 )
